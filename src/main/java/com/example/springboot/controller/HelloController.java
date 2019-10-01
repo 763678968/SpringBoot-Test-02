63d7bf305aa3c279f8a78cca8e51ee13b21ae2a9
animation <763678968@qq.com>
2019-10-01 23:28:32 +0800

    HelloController

--- src/main/java/com/example/springboot/controller/HelloController.java
+++ src/main/java/com/example/springboot/controller/HelloController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Arrays;
+import java.util.Map;
+
 @Controller
 public class HelloController {
 
@@ -11,5 +14,13 @@ public class HelloController {
     @RequestMapping("/hello")
     public String hello() {
         return "Hello World";
+    }
+
+    // 查出用户数据，在页面展示
+    @RequestMapping("/success")
+    public String success(Map<String, Object> map) {
+        map.put("hello", "<h1>你好</h1>");
+        map.put("users", Arrays.asList("zhangsan", "lisi", "wangwu"));
+        return "success";
     }
 }
