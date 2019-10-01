e28e9fb8917da6883f6d49d64452d6b2f2ff576b
animation <763678968@qq.com>
2019-10-01 23:28:10 +0800

    MyMvcConfig

---
+++ src/main/java/com/example/springboot/config/MyMvcConfig.java
@@ -0,0 +1,17 @@
+package com.example.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// 使用WebMvcConfigurer可以来扩展SpringMVC的功能
+@Configuration
+public class MyMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        // super.addViewControllers(registry)
+        // 浏览器发送 /atguigu 请求来到 success
+        registry.addViewController("/atguigu").setViewName("success");
+    }
+}
