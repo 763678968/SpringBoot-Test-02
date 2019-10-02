a3efccb00290f3f66ac664373ae846b878f33e0a
animation <763678968@qq.com>
2019-10-02 16:20:43 +0800

    接管SpringMVC

--- src/main/java/com/example/springboot/config/MyMvcConfig.java
+++ src/main/java/com/example/springboot/config/MyMvcConfig.java
@@ -1,10 +1,12 @@
 package com.example.springboot.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 // 使用WebMvcConfigurer可以来扩展SpringMVC的功能
+//@EnableWebMvc
 @Configuration
 public class MyMvcConfig implements WebMvcConfigurer {
 
